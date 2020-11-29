ab3fd09b0b30ff7e81308aaf3e18d0490e049324
kwatme <kwat.me@icloud.com>
2020-11-28 18:23:24 -0800

    GSEA-ing

--- src/sort_vectors.jl
+++
@@ -1,7 +0,0 @@
-function sort_vectors(vectors::Vector{Vector}; reverse::Bool=false,)
-
-    sort_indices = sortperm(vectors[1]; rev=reverse,)
-
-    return [vector[sort_indices] for vector in vectors]
-
-end
