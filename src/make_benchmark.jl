abea40eead5bbcab7b7bba5e96df7476be743ff5
kwatme <kwat.me@icloud.com>
2021-01-03 14:54:22 -0800

    seed

--- src/make_benchmark.jl
+++ src/make_benchmark.jl
@@ -1,21 +1,27 @@
-function make_benchmark(name)
+using StatsBase: sample
+
+function make_benchmark(id)
+
+    split_ = split(id)
     
-    if name == "abc"
+    if split_[1] == "card"
 
-        element_ = string.(collect('A':'Z'))
+        element_ = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "X"]
 
-        element_score_ = float.(collect(1:length(element_)))
+        element_score_ = float.(collect(-7:7))
 
-        set_element_ = string.(collect("KWAT"))
-
-    elseif name == "card"
-
-        element_ = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-
-        element_score_ = float.(collect(2:14))
-
-        set_element_ = ["8", "J"]
-
+        set_element_ = string.(collect(split_[2]))
+    
+    elseif split_[1] == "random" 
+            
+        element_ = ["e$index" for index in 1:parse(Int, split_[2])]
+        
+        n_element = length(element_)
+            
+        element_score_ = sort(randn(n_element))
+            
+        set_element_ = sample(element_, convert(Int64, floor(n_element * 0.1)))
+            
     end
 
     return element_, element_score_, set_element_
